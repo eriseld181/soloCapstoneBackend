8bbffddda02c9eed15a78afdb065788d70b7fb20
eriseld181 <49815928+eriseld181@users.noreply.github.com>
2020-09-17 23:27:53 +0200

    Create error handlers
    I have created these error handlers: 400, 403,  404 and  500 for general errors

---
+++ errorHandlers.js
@@ -0,0 +1,28 @@
+const badRequest = (error, req, res, next) => {
+    if (error.httpStatusCode === 400) {
+        res.status(400).send(error.message)
+    }
+    next(error)
+}
+const forbidden = (error, req, res, next) => {
+    if (error.httpStatusCode === 403) {
+        res.status(403).send(error.message || "You are forbidden, please contact admin!")
+    } next(error)
+}
+const notFound = (error, req, res, next) => {
+    if (error.httpStatusCode === 404) {
+        res.status(500).send(error.message || "Resource not found,please contact admin!")
+
+    } next(error)
+}
+const generalError = (error, req, res, next) => {
+    if (!res.headersSent) {
+        res.status(error.httpStatusCode || 500).send(error.message)
+    }
+}
+module.exports = {
+    badRequest,
+    forbidden,
+    notFound,
+    generalError
+}
