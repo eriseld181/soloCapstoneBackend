bce065db6ab0ccd4a6fcf95062d5aa23715f2650
eriseld181 <49815928+eriseld181@users.noreply.github.com>
2020-09-20 05:47:31 +0200

    Checked and fixed each field in schema
    Added username, grades, registerdate some default values in the beginning

--- source/server.js
+++ source/server.js
@@ -1,13 +1,20 @@
 const express = require('express')
 const cors = require('cors')
+const { join } = require("path")
 const listEndPoints = require('express-list-endpoints')
 const mongoose = require('mongoose')
-
+const userRouter = require('./services/users')
 
 
 const server = express()
+
 const port = process.env.PORT
+
+
 server.use(express.json())
+
+server.use("/api/users", userRouter)
+
 
 mongoose.connect(process.env.CONNECTION_DATABASE, {
     useNewUrlParser: true,
